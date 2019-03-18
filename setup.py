a4e12967a23636c6a028ab42044979d3bd55fbb4
Conrad Rosenbrock <rosenbrockc@gmail.com>
2019-03-18 16:38:54 -0600

    Add license for public release. Set release version at 0.4

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ except ImportError:
 
 from os import path
 setup(name='gblearn',
-      version='0.3',
+      version='0.4',
       description='Machine learning grain boundary properties and structure.',
       long_description= "" if not path.isfile("README.md") else read_md('README.md'),
       author='Conrad W Rosenbrock, Derek M Hensley',
